3ba07777226cef5f309f1809cfb594261ea4b78a
Cole Greenway <colegreenway@Coles-MacBook-Pro.local>
2022-08-12 18:53:27 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var hourlyWage float32 = 24.37
+var daysWorkedWeekly float32 = 4
+var daysOnVacation float32 = 12
+var hoursWorkedDaily float32 = 9.2
+var dailyPay = hourlyWage * hoursWorkedDaily
+
+func main() {
+	fmt.Println("You will be going on vacation for", +daysOnVacation, "days. Given your hourly wage of $", +hourlyWage, ", you will be receiving $", dailyPay*daysOnVacation, "less than your usual monthly check.")
+}
